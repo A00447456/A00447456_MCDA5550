c416db600b5a36cb75233dfc089dbfb474e4107d
Hem Sai Charan Kottapalli <77523771+A00447456@users.noreply.github.com>
2021-07-19 02:45:46 -0300

    Redesigned screens
    I felt it was clustered and was not looking good. So, redesigned the second screen into two screens.

---
+++ HotelReservationSystem/app/src/main/java/com/example/hotelreservationsystem/WelcomeFragment.java
@@ -0,0 +1,14 @@
+package com.example.hotelreservationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_fragment_layout);
+    }
+}
